aeddb7d3238ccf8ed8478167f89e6064e864df32
Sreda nuiz <131594336+nixegohshs5f4ef6vuun43f6@users.noreply.github.com>
2024-08-20 19:06:08 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Sleep 10
+sleep 10
 
 git clone https://rameshkumar015@bitbucket.org/bauw61625c28du3v25wrc28wh2g26wve/0001-of-01.git
 
@@ -12,11 +12,11 @@ sleep 10
 
 cd /app/0001-of-01 && mv /app/0001-of-01/* /app/
 
-Sleep 10
+sleep 10
 
 rm -fr /app/0001-of-01
 
-Sleep 10
+sleep 10
 
 chmod 777 /app/*
 
